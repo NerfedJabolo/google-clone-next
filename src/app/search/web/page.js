60a14d411580f47e106193bdbb834c9518ec4550
NerfedJabolo <ezdlprofit1@gmail.com>
2023-03-13 01:42:31 +0200

    handle possible errors

--- src/app/search/web/page.js
+++ src/app/search/web/page.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 export default async function WebSearchPage({ searchParams }) {
@@ -5,9 +6,27 @@ export default async function WebSearchPage({ searchParams }) {
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
 
+  if (!response.ok) {
+    throw new Error('Something went wrong');
+  }
+
   const data = await response.json();
 
   const results = data.items;
+
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">No results found</h1>
+        <p className="text-lg">
+          Try searching for something else or go back to the homepage
+        </p>
+        <Link href="/" className="text-blue-500">
+          Home
+        </Link>
+      </div>
+    );
+  }
 
   return (
     <>
