43a8702244445560d4f2f86cf47c22f2e8e59e37
NerfedJabolo <ezdlprofit1@gmail.com>
2023-03-12 19:49:56 +0200

    add home header component

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,9 @@
+import HomeHeader from '@/components/HomeHeader';
+
 export default function Home() {
-  return <div className="text-red-600 text-3xl">Home</div>;
+  return (
+    <>
+      <HomeHeader />
+    </>
+  );
 }
